01ba1b602fd463f64e428af5ee10f4d50011abf3
Manujdhull <manujdhull801@gmail.com>
2024-11-09 21:58:20 +0530

    optimised bubble sort

--- Day-1/Bubble-Sort.cpp
+++ Day-1/Bubble-Sort.cpp
@@ -17,14 +17,36 @@
 #include<algorithm>
 using namespace std;
 
+// int main(){
+//     int arr[] = {10, 9, 8, 17, 6, 15, 4, 33, 20, 1};
+//     int n=10;
+//     for(int i=1;i<n;i++){
+//         for(int j=0;j<n-i;j++){
+//             if(arr[j] > arr[j+1]){
+//                 swap(arr[j], arr[j+1]);
+//             }
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         cout<< arr[i] << " ";
+//     }
+// }
+
+// optimization bubble sort in O(n) if best case
+
 int main(){
     int arr[] = {10, 9, 8, 17, 6, 15, 4, 33, 20, 1};
     int n=10;
     for(int i=1;i<n;i++){
+        bool swapped = false;
         for(int j=0;j<n-i;j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
+        }
+        if(swapped == false){
+            // it means all the elements are sorted
+            break;
         }
     }
     for(int i=0;i<n;i++){
